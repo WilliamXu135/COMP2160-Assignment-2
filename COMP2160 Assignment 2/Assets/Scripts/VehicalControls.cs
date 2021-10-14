5a1a2ae7f48afd53f48b0f3a7a609185e272f4fa
WilliamXu135 <61869357+WilliamXu135@users.noreply.github.com>
2021-10-15 01:04:56 +1100

    Completed Car Controls

--- COMP2160 Assignment 2/Assets/Scripts/VehicalControls.cs
+++ COMP2160 Assignment 2/Assets/Scripts/VehicalControls.cs
@@ -5,70 +5,89 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class VehicalControls : MonoBehaviour
 {
-    private Rigidbody rigidbodyComponent;
-   
+    private Rigidbody rb;
+
     // Acceleration and turning force
     public float force;
-    public Vector3 turningForce;
+    public float turningForce;
 
-    private float velocity;                     // Car speed
-    private Vector3 rotate;                     // Degrees a second - car turning radius
-    private float velocityScaler;               // To scale the magnitude of velocity for
-                                                // controlling rotation
+    private float velocity;                             // Car speed
+    private float rotate;                               // Degrees a second - car turning radius
+    private float velScaler;                            // To scale the magnitude of velocity for
+                                                        // controlling rotation
 
     // To use in fixed update for controls
     private float accelerate;
-    private float turning;
-    
-    // Turning radius of car scaled with fixedDeltaTime
-    private Quaternion deltaRotation;
+    private float turn;
 
     // Used for detecting if the car is on the ground
-    private float clearanceError;
-    private bool grounded;
-    private float distanceToGround;
-    private Collider carCollider;
     public LayerMask layer;
+    private float distance;
+    private float dist;
+    private Vector3 transformOffset;
+    private float maxDistFromGround;
+    private float turnDirection;
 
     void Start()
     {
-        rigidbodyComponent = GetComponent<Rigidbody>();
-        carCollider = GetComponent<Collider>();
-        clearanceError = 0.1f;
-        velocityScaler = 10.0f;
-        distanceToGround = carCollider.bounds.extents.y + clearanceError;
+        rb = GetComponent<Rigidbody>();
+        velScaler = 10.0f;
+
+        transformOffset = new Vector3(0, 0.5f, 0);
+        distance = float.PositiveInfinity;
+        maxDistFromGround = 0.7f;
     }
 
     void Update()
     {
-        Debug.Log(distanceToGround);
         // Control Axis
         accelerate = Input.GetAxis(InputAxes.Vertical);
-        turning = Input.GetAxis(InputAxes.Horizontal);
+        turn = Input.GetAxis(InputAxes.Horizontal);
 
         velocity = accelerate * force;
-        rotate = turning * turningForce;
+       
+        if (accelerate <0)
+        {
+            turnDirection = -1;
+        }
+        else if(accelerate>0)
+        {
+            turnDirection = 1; 
+        }
+        rotate = turn * turningForce *turnDirection;
     }
 
     private void FixedUpdate()
     {
-        grounded = Physics.Raycast(rigidbodyComponent.position,
-             rigidbodyComponent.transform.TransformDirection(Vector3.down),
-             distanceToGround + clearanceError, layer);
+        float deltaTurnspeed;
 
-        if (grounded)
+        if (this.DistanceToGround() < maxDistFromGround)
         {
             // Car Accelerates with W and S keys with drag applied
-            rigidbodyComponent.AddRelativeForce(velocity * Time.fixedDeltaTime * Vector3.forward,ForceMode.Acceleration);
+            rb.AddRelativeForce(velocity * Time.fixedDeltaTime * Vector3.forward, ForceMode.Acceleration);
+            rb.AddRelativeForce(rb.drag * Time.fixedDeltaTime * Vector3.back);
 
-            rigidbodyComponent.AddForce(rigidbodyComponent.drag * 
-                rigidbodyComponent.angularDrag * Time.fixedDeltaTime * Vector3.back);
+            //Allows turning when W/S key not being pressed
+            rotate *= (rb.velocity.magnitude / velScaler);
+            deltaTurnspeed = rotate * Time.fixedDeltaTime;
 
-            // Car turning with A / D keys
-            deltaRotation = Quaternion.Euler(rotate * Time.fixedDeltaTime *
-                rigidbodyComponent.velocity.magnitude / velocityScaler);
-
-            rigidbodyComponent.MoveRotation(rigidbodyComponent.rotation * deltaRotation);
+            rb.AddRelativeTorque(deltaTurnspeed * Vector3.up, ForceMode.VelocityChange);
         }
+        // Stop car from sliding
     }
+
+    private float DistanceToGround()
+    {
+        Vector3 pos = transform.position + transformOffset;
+        Vector3 dir = Vector3.down;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(pos, dir, out hit, distance, layer))
+        {
+            dist = hit.distance;
+        }
+        return dist;
+    }
+
 }
