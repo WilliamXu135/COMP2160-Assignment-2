7bfde9c26782fd86e5654a94adabf06939297ec3
WilliamXu135 <61869357+WilliamXu135@users.noreply.github.com>
2021-10-14 16:55:22 +1100

    Controls only when grounded (Incomplete) - Safety push due to thunderstorm

--- COMP2160 Assignment 2/Assets/Scripts/VehicalControls.cs
+++ COMP2160 Assignment 2/Assets/Scripts/VehicalControls.cs
@@ -11,43 +11,68 @@ public class VehicalControls : MonoBehaviour
     public float force;
     public Vector3 turningForce;
 
-
-    private float velocity;             // Car speed
-    private Vector3 rotate;             //Degrees a second - car turning radius
+    private float velocity;                     // Car speed
+    private Vector3 rotate;                     // Degrees a second - car turning radius
+    private float velocityScaler;               // To scale the magnitude of velocity for
+                                                // controlling rotation
 
     // To use in fixed update for controls
     private float accelerate;
     private float turning;
-
-
+    
+    // Turning radius of car scaled with fixedDeltaTime
     private Quaternion deltaRotation;
+
+    // Used for detecting if the car is on the ground
+    private float clearanceError;
+    private bool grounded;
+    private float distanceToGround;
+    private Collider carCollider;
+    public LayerMask layer;
 
     void Start()
     {
         rigidbodyComponent = GetComponent<Rigidbody>();
+        carCollider = GetComponent<Collider>();
+        clearanceError = 0.1f;
+        velocityScaler = 10.0f;
+        distanceToGround = carCollider.bounds.extents.y + clearanceError;
     }
 
     void Update()
     {
+        Debug.Log(distanceToGround);
         // Control Axis
         accelerate = Input.GetAxis(InputAxes.Vertical);
         turning = Input.GetAxis(InputAxes.Horizontal);
 
-
+        RaycastHit hit;
         velocity = accelerate * force;
         rotate = turning * turningForce;
+        grounded = Physics.Raycast(rigidbodyComponent.position,
+             rigidbodyComponent.transform.TransformDirection(Vector3.down),
+             out hit, distanceToGround + clearanceError, layer) ;
+
+        Debug.Log(hit);
+
     }
 
     private void FixedUpdate()
     {
 
+        if (grounded)
+        {
+            // Car Accelerates with W and S keys with drag applied
+            rigidbodyComponent.AddRelativeForce(velocity * Time.fixedDeltaTime * Vector3.forward,ForceMode.Acceleration);
 
-        // Car Accelerates with W and S keys with drag applied
-        rigidbodyComponent.AddRelativeForce(velocity * Time.fixedDeltaTime * Vector3.forward);
-        rigidbodyComponent.AddForce(rigidbodyComponent.drag * Time.fixedDeltaTime * Vector3.back);
+            rigidbodyComponent.AddForce(rigidbodyComponent.drag * 
+                rigidbodyComponent.angularDrag * Time.fixedDeltaTime * Vector3.back);
 
-        // Car turning with A / D keys
-        deltaRotation = Quaternion.Euler(rotate * Time.fixedDeltaTime);
-        rigidbodyComponent.MoveRotation(rigidbodyComponent.rotation * deltaRotation);
+            // Car turning with A / D keys
+            deltaRotation = Quaternion.Euler(rotate * Time.fixedDeltaTime *
+                rigidbodyComponent.velocity.magnitude / velocityScaler);
+
+            rigidbodyComponent.MoveRotation(rigidbodyComponent.rotation * deltaRotation);
+        }
     }
 }
